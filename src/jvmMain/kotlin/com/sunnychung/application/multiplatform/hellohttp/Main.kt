aa81ff6643b7c4b9972d4cc5e808262a3e8cd068
Sunny Chung <sunnychung@live.hk>
2023-10-09 20:59:29 +0800

    update the window to display title bar in dark under OS dark mode

--- src/jvmMain/kotlin/com/sunnychung/application/multiplatform/hellohttp/Main.kt
+++ src/jvmMain/kotlin/com/sunnychung/application/multiplatform/hellohttp/Main.kt
@@ -4,8 +4,11 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.sunnychung.application.multiplatform.hellohttp.ux.AppView
 
-fun main() = application {
+fun main() {
+    System.setProperty("apple.awt.application.appearance", "system")
+    application {
     Window(title = "Hello HTTP", onCloseRequest = ::exitApplication) {
         AppView()
+    }
     }
 }
