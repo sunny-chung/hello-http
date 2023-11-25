0bc5f031be2dd80fbc3f800e375d9f82d1b62519
Sunny Chung <sunnychung@live.hk>
2023-11-25 18:45:01 +0800

    fix crash or UI freeze when large number of lines are loaded to Transport Timeline

--- src/jvmMain/kotlin/com/sunnychung/application/multiplatform/hellohttp/ux/TransportTimelineView.kt
+++ src/jvmMain/kotlin/com/sunnychung/application/multiplatform/hellohttp/ux/TransportTimelineView.kt
@@ -3,18 +3,23 @@ package com.sunnychung.application.multiplatform.hellohttp.ux
 import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.foundation.v2.ScrollbarAdapter
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.LocalTextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -34,6 +39,7 @@ import androidx.compose.ui.unit.dp
 import com.sunnychung.application.multiplatform.hellohttp.model.ProtocolVersion
 import com.sunnychung.application.multiplatform.hellohttp.model.RawExchange
 import com.sunnychung.application.multiplatform.hellohttp.util.log
+import com.sunnychung.application.multiplatform.hellohttp.ux.compose.rememberLast
 import com.sunnychung.application.multiplatform.hellohttp.ux.local.LocalColor
 import com.sunnychung.application.multiplatform.hellohttp.ux.local.LocalFont
 import com.sunnychung.lib.multiplatform.kdatetime.KDateTimeFormat
@@ -46,8 +52,6 @@ private val TIMESTAMP_COLUMN_WIDTH_DP = 130.dp
 @Composable
 fun TransportTimelineView(modifier: Modifier = Modifier, protocol: ProtocolVersion?, exchange: RawExchange) {
     val timestampColumnWidthDp = TIMESTAMP_COLUMN_WIDTH_DP
-    val scrollState = rememberLazyListState()
-//    val scrollState = rememberScrollState()
 
     log.d { "TransportTimelineView recompose" }
 
@@ -102,120 +106,221 @@ fun TransportTimelineView(modifier: Modifier = Modifier, protocol: ProtocolVersi
         }
         log.d { "numCharsInALine = $numCharsInALine" }
 
-        SelectionContainer {
-            LazyColumn(state = scrollState) {
-                synchronized(exchange.exchanges) {
-//                    items(items = exchange.exchanges) {
-//            Column(modifier = Modifier.verticalScroll(scrollState)) {
-//                synchronized(exchange.exchanges) {
-                        exchange.exchanges.forEachIndexed { index, it ->
-                            var text = it.detail ?: it.payload?.decodeToString()
-                            ?: it.payloadBuilder?.toByteArray()
-                                ?.decodeToString() ?: "<Payload Lost>"
-//                            text = text.substring(0 .. minOf(1000, text.length - 1)) + " (${text.length} length)"
+        var totalNumLines by rememberLast(exchange.uiVersion) { mutableStateOf<Int?>(null) }
 
-                            // workaround this Compose bug:
-                            // https://github.com/JetBrains/compose-multiplatform/issues/2420
-                            val (textsSplitted, numTextLines) = if (contentWidthInPx != null && numCharsInALine > 0) {
-//                                remember(text, numCharsInALine) {
-                                    val splitted = text.split('\n')
-                                        .flatMap {
-                                            if (it.isNotEmpty()) {
-                                                it.chunked(numCharsInALine)
-                                            } else {
-                                                listOf("")
-                                            }.toMutableList().apply {
-                                                this[lastIndex] += "\n"
-                                            }
-                                        }
-                                        .withIndex()
-//                                        .groupBy { it.index / 100 }
-                                        .groupBy { it.index / 1 } // word wrap breaks maxLines of grouped strings
+        val scrollbarAdapter: ScrollbarAdapter
+        /*
+            Large number of Text elements freezes scrolling and rendering. In this situation LazyColumn helps.
+            But `SelectionContainer { LazyColumn { ... } }` cannot select text elements span over a screen.
+            (See https://github.com/JetBrains/compose-multiplatform/issues/3550)
 
-                                    Pair(
-                                        splitted.map { it.value.joinToString("") { it.value } },
-                                        splitted.map { it.value.size },
-                                    )
-//                                }
-                            } else {
-                                Pair(listOf(" "), listOf(1))
-                            }
-                            log.d { "max chars = " + textsSplitted.maxOf { it.length } }
+            So, Column is still being used when the number of Text elements is small.
+         */
+        if ((totalNumLines ?: Int.MAX_VALUE) > 2000) {
+            val scrollState = rememberLazyListState()
+            scrollbarAdapter = rememberScrollbarAdapter(scrollState)
 
-                            textsSplitted.forEachIndexed { textIndex, textChunk ->
-                                item(key = "$protocol-$index-$textIndex-${textChunk.hashCode()}") {
-                                    // Not using `height(IntrinsicSize.Min)` because it is buggy.
-                                    Row(modifier = Modifier.padding(
-                                        start = 6.dp,
-                                        end = 6.dp,
-                                        top = if (textIndex == 0) 2.dp else 0.dp,
-                                        bottom = if (textIndex == textsSplitted.lastIndex) 2.dp else 0.dp
-                                    )) {
-                                        DisableSelection {
-                                            if (textIndex == 0) {
-                                                TimestampColumn(
-                                                    createTime = it.instant,
-                                                    lastUpdateTime = it.lastUpdateInstant,
-                                                    modifier = Modifier.width(TIMESTAMP_COLUMN_WIDTH_DP)
-                                                        .padding(end = 1.dp)
-                                                )
-                                                AppText(
-                                                    text = when (it.direction) {
-                                                        RawExchange.Direction.Outgoing -> "> "
-                                                        RawExchange.Direction.Incoming -> "< "
-                                                        else -> "= "
-                                                    },
-                                                    fontFamily = FontFamily.Monospace,
-                                                    modifier = Modifier.padding(start = 4.dp)
-                                                )
-                                                // cannot filter by it.direction != RawExchange.Direction.Unspecified
-                                                // otherwise contentWidthInPx keeps changing and causes infinite recompose loops and view overlapping
-                                                if (protocol?.isHttp2() == true) {
-                                                    AppText(
-                                                        text = "{${(it.streamId?.toString() ?: "*")}}".padStart(2 + streamDigits, ' ') + " ",
-                                                        fontFamily = FontFamily.Monospace,
-                                                    )
-                                                }
-                                            } else {
-                                                Spacer(Modifier.width(TIMESTAMP_COLUMN_WIDTH_DP)
-                                                    .padding(end = 1.dp))
-                                                AppText(
-                                                    text = "  ",
-                                                    fontFamily = FontFamily.Monospace,
-                                                    modifier = Modifier.padding(start = 4.dp)
-                                                )
-                                                // cannot filter by it.direction != RawExchange.Direction.Unspecified
-                                                // otherwise contentWidthInPx keeps changing and causes infinite recompose loops and view overlapping
-                                                if (protocol?.isHttp2() == true) {
-                                                    AppText(
-                                                        text = " ".repeat("{} ".length + streamDigits),
-                                                        fontFamily = FontFamily.Monospace,
-                                                    )
-                                                }
-                                            }
-                                        }
-                                        AppText(
-                                            text = textChunk,
-                                            fontFamily = FontFamily.Monospace,
-                                            maxLines = numTextLines[textIndex],
-                                            modifier = Modifier.weight(1f).onGloballyPositioned {
-                                                contentWidthInPx = it.size.width
-                                            },
-                                        )
-                                    }
-                                }
-                            }
+            log.d { "TransportTimelineView adopts LazyColumn $totalNumLines" }
 
-//                        }
+            SelectionContainer {
+                LazyColumn(state = scrollState) {
+                    synchronized(exchange.exchanges) {
+    //                    items(items = exchange.exchanges) {
+                        TransportTimelineContentView(
+                            scope = this,
+                            exchange = exchange,
+                            contentWidthInPx = contentWidthInPx,
+                            numCharsInALine = numCharsInALine,
+                            protocol = protocol,
+                            streamDigits = streamDigits,
+                            onMeasureContentWidth = { contentWidthInPx = it },
+                            onMeasureContentLines = { totalNumLines = it.totalNumLines },
+                            onPrepareComposable = {},
+                        )
+    //                    }
                     }
+                }
+            }
+        } else {
+            val scrollState = rememberScrollState()
+            scrollbarAdapter = rememberScrollbarAdapter(scrollState)
+
+            log.d { "TransportTimelineView adopts Column $totalNumLines" }
+
+            SelectionContainer {
+                Column(modifier = Modifier.verticalScroll(scrollState)) {
+                    val composables = mutableListOf<@Composable () -> Unit>()
+                    synchronized(exchange.exchanges) {
+                        TransportTimelineContentView(
+                            scope = this,
+                            exchange = exchange,
+                            contentWidthInPx = contentWidthInPx,
+                            numCharsInALine = numCharsInALine,
+                            protocol = protocol,
+                            streamDigits = streamDigits,
+                            onMeasureContentWidth = { contentWidthInPx = it },
+                            onMeasureContentLines = { totalNumLines = it.totalNumLines },
+                            onPrepareComposable = { composables += it },
+                        )
+                    }
+                    composables.forEach { it() }
                 }
             }
         }
 
         VerticalScrollbar(
             modifier = Modifier.align(Alignment.CenterEnd),
-            adapter = rememberScrollbarAdapter(scrollState),
+            adapter = scrollbarAdapter,
         )
+    }
+}
+
+private fun TransportTimelineContentView(
+    scope: Any,
+    exchange: RawExchange,
+    contentWidthInPx: Int?,
+    numCharsInALine: Int,
+    protocol: ProtocolVersion?,
+    streamDigits: Int,
+    onMeasureContentWidth: (Int) -> Unit,
+    onMeasureContentLines: (TransportTimelineContentMeasureResult) -> Unit,
+    onPrepareComposable: (@Composable () -> Unit) -> Unit,
+) {
+    var totalLines = 0
+    exchange.exchanges.forEachIndexed { index, it ->
+        var text = it.detail ?: it.payload?.decodeToString()
+        ?: it.payloadBuilder?.toByteArray()
+            ?.decodeToString() ?: "<Payload Lost>"
+//      text = text.substring(0 .. minOf(1000, text.length - 1)) + " (${text.length} length)"
+
+        // workaround this Compose bug:
+        // https://github.com/JetBrains/compose-multiplatform/issues/2420
+        val (textsSplitted, numTextLines) = if (contentWidthInPx != null && numCharsInALine > 0) {
+//        remember(text, numCharsInALine) {
+            val splitted = text.split('\n')
+                .flatMap {
+                    if (it.isNotEmpty()) {
+                        it.chunked(numCharsInALine)
+                    } else {
+                        listOf("")
+                    }.toMutableList().apply {
+                        this[lastIndex] += "\n"
+                    }
+                }
+                .withIndex()
+//                .groupBy { it.index / 100 }
+                .groupBy { it.index / 1 } // word wrap breaks maxLines of grouped strings
+
+            Pair(
+                splitted.map { it.value.joinToString("") { it.value } },
+                splitted.map { it.value.size },
+            )
+//        }
+        } else {
+            Pair(listOf(" "), listOf(1))
+        }
+        totalLines += textsSplitted.size
+        log.d { "max chars = " + textsSplitted.maxOf { it.length } }
+        log.d { "size = ${textsSplitted.size}" }
+
+        textsSplitted.forEachIndexed { textIndex, textChunk ->
+            lazyOrNormalItem(scope = scope, key = "$protocol-$index-$textIndex-${textChunk.hashCode()}", onPrepareComposable = onPrepareComposable) {
+                // Not using `height(IntrinsicSize.Min)` because it is buggy.
+                Row(
+                    modifier = Modifier.padding(
+                        start = 6.dp,
+                        end = 6.dp,
+                        top = if (textIndex == 0) 2.dp else 0.dp,
+                        bottom = if (textIndex == textsSplitted.lastIndex) 2.dp else 0.dp
+                    )
+                ) {
+                    DisableSelection {
+                        if (textIndex == 0) {
+                            TimestampColumn(
+                                createTime = it.instant,
+                                lastUpdateTime = it.lastUpdateInstant,
+                                modifier = Modifier.width(TIMESTAMP_COLUMN_WIDTH_DP)
+                                    .padding(end = 1.dp)
+                            )
+                            AppText(
+                                text = when (it.direction) {
+                                    RawExchange.Direction.Outgoing -> "> "
+                                    RawExchange.Direction.Incoming -> "< "
+                                    else -> "= "
+                                },
+                                fontFamily = FontFamily.Monospace,
+                                modifier = Modifier.padding(start = 4.dp)
+                            )
+                            // cannot filter by it.direction != RawExchange.Direction.Unspecified
+                            // otherwise contentWidthInPx keeps changing and causes infinite recompose loops and view overlapping
+                            if (protocol?.isHttp2() == true) {
+                                AppText(
+                                    text = "{${(it.streamId?.toString() ?: "*")}}".padStart(
+                                        2 + streamDigits,
+                                        ' '
+                                    ) + " ",
+                                    fontFamily = FontFamily.Monospace,
+                                )
+                            }
+                        } else {
+                            Spacer(
+                                Modifier.width(TIMESTAMP_COLUMN_WIDTH_DP)
+                                    .padding(end = 1.dp)
+                            )
+                            AppText(
+                                text = "  ",
+                                fontFamily = FontFamily.Monospace,
+                                modifier = Modifier.padding(start = 4.dp)
+                            )
+                            // cannot filter by it.direction != RawExchange.Direction.Unspecified
+                            // otherwise contentWidthInPx keeps changing and causes infinite recompose loops and view overlapping
+                            if (protocol?.isHttp2() == true) {
+                                AppText(
+                                    text = " ".repeat("{} ".length + streamDigits),
+                                    fontFamily = FontFamily.Monospace,
+                                )
+                            }
+                        }
+                    }
+                    AppText(
+                        text = textChunk,
+                        fontFamily = FontFamily.Monospace,
+                        maxLines = numTextLines[textIndex],
+                        softWrap = false, // since maxLines is always 1
+                        modifier = Modifier.weight(1f).onGloballyPositioned {
+                            onMeasureContentWidth(it.size.width)
+                        },
+                    )
+                }
+            }
+        }
+
+    }
+
+    if (contentWidthInPx != null && numCharsInALine > 0) {
+        onMeasureContentLines(
+            TransportTimelineContentMeasureResult(
+                measureInputWidthPx = contentWidthInPx,
+                totalNumLines = totalLines
+            )
+        )
+    }
+}
+
+data class TransportTimelineContentMeasureResult(val measureInputWidthPx: Int?, val totalNumLines: Int)
+
+fun lazyOrNormalItem(
+    scope: Any,
+    key: Any?,
+    onPrepareComposable: (@Composable () -> Unit) -> Unit,
+    content: @Composable () -> Unit,
+) {
+    when (scope) {
+        is LazyListScope -> scope.item(key = key) {
+            content()
+        }
+        is ColumnScope -> onPrepareComposable(content)
+        else -> throw UnsupportedOperationException()
     }
 }
 
