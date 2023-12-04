e40eab8139c78406f1a817172680de868116c4ae
Sunny Chung <sunnychung@live.hk>
2023-12-04 21:32:00 +0800

    update version to 1.4.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "com.sunnychung.application"
-version = "1.4.0" // must be in 'x.y.z' for native distributions
+version = "1.4.1-SNAPSHOT" // must be in 'x.y.z' for native distributions
 
 repositories {
     google()
