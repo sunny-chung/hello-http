15688f743cfa2cfedca5a2ef16d3af2f7b0d6372
Sunny Chung <sunnychung@live.hk>
2023-12-20 21:22:31 +0800

    update version to 1.5.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "com.sunnychung.application"
-version = "1.4.3" // must be in 'x.y.z' for native distributions
+version = "1.5.0-SNAPSHOT" // must be in 'x.y.z' for native distributions
 
 repositories {
     google()
