076ca3b9021b6e559e42088244c9995c43fb6875
Sunny Chung <sunnychung@live.hk>
2024-05-15 19:35:49 +0800

    fix CI

---
+++ run-ci-test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+./gradlew bootRun &
+
+READY=false
+for attempt in {1..20}; do sleep 1; if curl -sI --fail-early -f http://localhost:18081/actuator/health/; then READY=true; break; fi; echo "Waiting for Test Server to be UP"; done
+[ "$READY" = true ] || (echo "Test Server is not UP" && exit 1)
+echo "Test Server is UP"
+
+./gradlew check -PisCI=true
