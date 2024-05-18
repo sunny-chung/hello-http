801a43ec7a9c800fe8fddb9f1577ebf51cdae25e
Sunny Chung <sunnychung@live.hk>
2024-05-18 11:55:24 +0800

    update CI script to kill test server at the end of test execution

--- run-ci-test.sh
+++ run-ci-test.sh
@@ -4,6 +4,13 @@ set -e
 
 ./gradlew :test-server:bootJar
 ./gradlew :test-server:bootRun &
+TEST_SERVER_PID=$!
+
+function cleanup {
+  [ "$TEST_SERVER_PID" ] && kill $TEST_SERVER_PID
+}
+
+trap cleanup EXIT
 
 READY=false
 for attempt in {1..20}; do sleep 1; if curl -sI --fail-early -f http://localhost:18081/actuator/health/; then READY=true; break; fi; echo "Waiting for Test Server to be UP"; done
