0a90d03257047cc82b4d4c0c6a41788bba5a3476
Sunny Chung <sunnychung@live.hk>
2024-05-18 15:51:32 +0800

    update CI script not to use Gradle daemon in attempt to fix OutOfMemoryError during UX tests

--- run-ci-test.sh
+++ run-ci-test.sh
@@ -3,7 +3,8 @@
 set -e
 
 ./gradlew :test-server:bootJar
-./gradlew :test-server:bootRun &
+
+GRADLE_OPTS='-Xmx64m -Dorg.gradle.daemon=false -Dorg.gradle.jvmargs="-Xmx500m"' ./gradlew :test-server:bootRun &
 TEST_SERVER_PID=$!
 
 function cleanup {
@@ -13,10 +14,9 @@ function cleanup {
 trap cleanup EXIT
 
 READY=false
-for attempt in {1..20}; do sleep 1; if curl -sI --fail-early -f http://localhost:18081/actuator/health/; then READY=true; break; fi; echo "Waiting for Test Server to be UP"; done
+for attempt in {1..30}; do sleep 1; if curl -sI --fail-early -f http://localhost:18081/actuator/health/; then READY=true; break; fi; echo "Waiting for Test Server to be UP"; done
 [ "$READY" = true ] || (echo "Test Server is not UP" && exit 1)
 echo "Test Server is UP"
 
-export JAVA_OPTS='"-Xmx2048m"'
-export GRADLE_OPTS='"-Dorg.gradle.jvmargs=-Xmx2048m"'
+export GRADLE_OPTS='-Xmx64m -Dorg.gradle.daemon=false -Dorg.gradle.jvmargs="-Xmx2048m"'
 ./gradlew check -PisCI=true
