d09690fbe975a8bd55bdbf0a22977f7d033eae1a
Sunny Chung <sunnychung@live.hk>
2024-05-24 23:56:35 +0800

    fix ConcurrentModificationException

--- src/jvmMain/kotlin/com/sunnychung/application/multiplatform/hellohttp/network/AbstractTransportClient.kt
+++ src/jvmMain/kotlin/com/sunnychung/application/multiplatform/hellohttp/network/AbstractTransportClient.kt
@@ -263,12 +263,6 @@ abstract class AbstractTransportClient internal constructor(callDataStore: CallD
         assert(callData.isPrepared)
     }
 
-    protected fun CallData.consumePayloads() {
-        response.rawExchange.exchanges.forEachIndexed { index, it ->
-            it.consumePayloadBuilder(isComplete = index < response.rawExchange.exchanges.lastIndex)
-        }
-    }
-
     fun executePostFlightAction(callId: String, out: UserResponse, postFlightAction: ((UserResponse) -> Unit)) {
         emitEvent(callId, "Executing Post Flight Actions")
         try {
@@ -277,6 +271,14 @@ abstract class AbstractTransportClient internal constructor(callDataStore: CallD
         } catch (e: Throwable) {
             out.postFlightErrorMessage = e.message
             emitEvent(callId, "Post Flight Actions Stopped with Error -- ${e.message}")
+        }
+    }
+}
+
+fun CallData.consumePayloads(isComplete: Boolean = false) {
+    synchronized(response.rawExchange.exchanges) {
+        response.rawExchange.exchanges.forEachIndexed { index, it ->
+            it.consumePayloadBuilder(isComplete = isComplete || index < response.rawExchange.exchanges.lastIndex)
         }
     }
 }
