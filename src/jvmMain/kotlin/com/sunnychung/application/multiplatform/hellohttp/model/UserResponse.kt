531c4af1c9079304dbc1d8daf3768523b2a61232
Sunny Chung <sunnychung@live.hk>
2024-06-01 17:58:52 +0800

    fix truncation notice was missing in copy text of transport timeline

--- src/jvmMain/kotlin/com/sunnychung/application/multiplatform/hellohttp/model/UserResponse.kt
+++ src/jvmMain/kotlin/com/sunnychung/application/multiplatform/hellohttp/model/UserResponse.kt
@@ -288,8 +288,15 @@ fun UserResponse.describeTransportLayer(isRelativeTimeDisplay: Boolean) = buildS
             },
             it.streamId?.toString() ?: if (protocol?.isHttp2() == true) "*" else "",
             it.detail
-                ?: it.payload?.decodeToString()
-                ?: it.payloadBuilder?.toByteArray()?.decodeToString()
+                ?: (it.payload ?: it.payloadBuilder?.toByteArray())
+                    ?.let { bytes ->
+                        val text = bytes.decodeToString()
+                        if (bytes.size < (it.payloadSize ?: 0)) {
+                            "$text ...(truncated, total size: ${DecimalFormat("#,###").format(it.payloadSize)} bytes)"
+                        } else {
+                            text
+                        }
+                    }
                 ?: "<Payload Lost>",
         )
     }
