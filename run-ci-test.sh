2e5634b5ee4d9ca6873f5bea452a2081110b0998
Sunny Chung <sunnychung@live.hk>
2024-06-09 12:10:19 +0800

    add health check of all test servers before test run, to make sure the test environment is ready even on a slow runner

--- run-ci-test.sh
+++ run-ci-test.sh
@@ -46,5 +46,20 @@ for attempt in {1..60}; do sleep 1; if curl -sI --fail-early -f http://localhost
 [ "$READY" = true ] || (echo "Test Server (HTTP/1) is not UP" && exit 1)
 echo "Test Server (HTTP/1) is UP"
 
+READY=false
+for attempt in {1..60}; do sleep 1; if curl -sI --insecure --fail-early -f https://localhost:18088/actuator/health/; then READY=true; break; fi; echo "Waiting for Test Server (HTTP/1) to be UP"; done
+[ "$READY" = true ] || (echo "Test Server (HTTP/1 SSL) is not UP" && exit 1)
+echo "Test Server (HTTP/1 SSL) is UP"
+
+READY=false
+for attempt in {1..60}; do sleep 1; if curl -sI --insecure  --fail-early -f https://localhost:18084/actuator/health/; then READY=true; break; fi; echo "Waiting for Test Server (HTTP/1) to be UP"; done
+[ "$READY" = true ] || (echo "Test Server (SSL) is not UP" && exit 1)
+echo "Test Server (SSL) is UP"
+
+READY=false
+for attempt in {1..60}; do sleep 1; if curl -sI --insecure --cert ./test-common/src/main/resources/tls/clientKeyAndCert.pem --fail-early -f https://localhost:18086/actuator/health/; then READY=true; break; fi; echo "Waiting for Test Server (HTTP/1) to be UP"; done
+[ "$READY" = true ] || (echo "Test Server (mTLS) is not UP" && exit 1)
+echo "Test Server (mTLS) is UP"
+
 export GRADLE_OPTS='-Xmx64m -Dorg.gradle.daemon=false -Dorg.gradle.jvmargs="-Xmx3072m -XX:+HeapDumpOnOutOfMemoryError"'
 ./gradlew check -PisCI=true
