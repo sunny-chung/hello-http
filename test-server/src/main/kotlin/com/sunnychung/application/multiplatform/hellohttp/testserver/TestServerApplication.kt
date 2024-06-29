27f38fb3a1cc05254e0671c76729bb8af1195264
Sunny Chung <sunnychung@live.hk>
2024-06-29 16:37:03 +0800

    debug test server OOM

--- test-server/src/main/kotlin/com/sunnychung/application/multiplatform/hellohttp/testserver/TestServerApplication.kt
+++ test-server/src/main/kotlin/com/sunnychung/application/multiplatform/hellohttp/testserver/TestServerApplication.kt
@@ -8,7 +8,9 @@ class TestServerApplication
 
 fun main(args: Array<String>) {
 	println(">>>> io.netty.maxDirectMemory = ${System.getProperty("io.netty.maxDirectMemory")}")
-	println(">>>> maxDirectMemory = ${io.netty.util.internal.PlatformDependent.maxDirectMemory()}")
+	println(">>>> netty maxDirectMemory = ${io.netty.util.internal.PlatformDependent.maxDirectMemory()}")
+	println(">>>> runtime max memory = ${Runtime.getRuntime().maxMemory()}")
+	println(">>>> netty estimateMaxDirectMemory = ${io.netty.util.internal.PlatformDependent.estimateMaxDirectMemory()}")
 
 	System.setProperty("reactor.netty.http.server.accessLogEnabled", "true")
 	runApplication<TestServerApplication>(*args)
