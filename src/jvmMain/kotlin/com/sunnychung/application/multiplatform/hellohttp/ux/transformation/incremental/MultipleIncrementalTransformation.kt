2ab1f34ba0bcd2de6fdfb04c65c42a230ea84c35
Sunny Chung <sunnychung@live.hk>
2024-10-02 00:50:22 +0800

    fix missing file

---
+++ src/jvmMain/kotlin/com/sunnychung/application/multiplatform/hellohttp/ux/transformation/incremental/MultipleIncrementalTransformation.kt
@@ -0,0 +1,22 @@
+package com.sunnychung.application.multiplatform.hellohttp.ux.transformation.incremental
+
+import com.sunnychung.application.multiplatform.hellohttp.ux.bigtext.BigText
+import com.sunnychung.application.multiplatform.hellohttp.ux.bigtext.BigTextChangeEvent
+import com.sunnychung.application.multiplatform.hellohttp.ux.bigtext.BigTextTransformer
+import com.sunnychung.application.multiplatform.hellohttp.ux.bigtext.IncrementalTextTransformation
+
+class MultipleIncrementalTransformation(val transformations: List<IncrementalTextTransformation<*>>) : IncrementalTextTransformation<Any?> {
+    override fun initialize(text: BigText, transformer: BigTextTransformer): Any? {
+        transformations.forEach {
+            it.initialize(text, transformer)
+        }
+        return Unit
+    }
+
+    override fun onTextChange(change: BigTextChangeEvent, transformer: BigTextTransformer, context: Any?) {
+        transformations.forEach {
+            (it as IncrementalTextTransformation<Any?>).onTextChange(change, transformer, context)
+        }
+    }
+
+}
