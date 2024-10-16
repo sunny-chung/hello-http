8708aba41b318886a4e22356a2a292597fa65822
Sunny Chung <sunnychung@live.hk>
2024-10-16 23:57:18 +0800

    refactor by extracting BigTextViewState class to a separated source file

---
+++ src/jvmMain/kotlin/com/sunnychung/application/multiplatform/hellohttp/ux/bigtext/BigTextViewState.kt
@@ -0,0 +1,130 @@
+package com.sunnychung.application.multiplatform.hellohttp.ux.bigtext
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.TransformedText
+
+class BigTextViewState {
+    /**
+     * A unique value that changes when the BigText string value is changed.
+     *
+     * This field is generated randomly and is NOT a sequence number.
+     */
+    var version: Long by mutableStateOf(0)
+        internal set
+
+    var firstVisibleRow: Int by mutableStateOf(0)
+        internal set
+
+    var lastVisibleRow: Int by mutableStateOf(0)
+        internal set
+
+    internal var transformedSelection: IntRange by mutableStateOf(0..-1)
+
+    /**
+     * `transformedSelectionStart` can be different from `transformedSelection.start`.
+     * If a text is selected from position 5 to 1, transformedSelection = (1 .. 5) while transformedSelectionStart = 5.
+     */
+    var transformedSelectionStart: Int by mutableStateOf(0)
+
+    var selection: IntRange by mutableStateOf(0..-1)
+
+    fun hasSelection(): Boolean = !transformedSelection.isEmpty()
+
+    internal fun updateSelectionByTransformedSelection(transformedText: TransformedText) {
+        selection = transformedText.offsetMapping.transformedToOriginal(transformedSelection.first) ..
+                transformedText.offsetMapping.transformedToOriginal(transformedSelection.last)
+    }
+
+    internal fun updateTransformedSelectionBySelection(transformedText: TransformedText) {
+        transformedSelection = transformedText.offsetMapping.originalToTransformed(selection.first) ..
+                transformedText.offsetMapping.originalToTransformed(selection.last)
+    }
+
+    internal fun updateSelectionByTransformedSelection(transformedText: BigTextTransformed) {
+        selection = transformedText.findOriginalPositionByTransformedPosition(transformedSelection.first) ..
+                transformedText.findOriginalPositionByTransformedPosition(transformedSelection.last)
+    }
+
+    internal fun updateTransformedSelectionBySelection(transformedText: BigTextTransformed) {
+        transformedSelection = transformedText.findTransformedPositionByOriginalPosition(selection.first) ..
+                transformedText.findTransformedPositionByOriginalPosition(selection.last)
+    }
+
+    internal var transformedCursorIndex by mutableStateOf(0)
+    var cursorIndex by mutableStateOf(0)
+
+    fun updateCursorIndexByTransformed(transformedText: TransformedText) {
+        cursorIndex = transformedText.offsetMapping.transformedToOriginal(transformedCursorIndex)
+    }
+
+    fun updateTransformedCursorIndexByOriginal(transformedText: TransformedText) {
+        transformedCursorIndex = transformedText.offsetMapping.originalToTransformed(cursorIndex)
+    }
+
+    fun updateCursorIndexByTransformed(transformedText: BigTextTransformed) {
+        cursorIndex = transformedText.findOriginalPositionByTransformedPosition(transformedCursorIndex).also {
+            com.sunnychung.application.multiplatform.hellohttp.util.log.d { "cursorIndex = $it (from T $transformedCursorIndex)" }
+        }
+    }
+
+    fun updateTransformedCursorIndexByOriginal(transformedText: BigTextTransformed) {
+        transformedCursorIndex = transformedText.findTransformedPositionByOriginalPosition(cursorIndex).also {
+            com.sunnychung.application.multiplatform.hellohttp.util.log.d { "updateTransformedCursorIndexByOriginal = $it (from $cursorIndex)" }
+        }
+        cursorIndex = transformedText.findOriginalPositionByTransformedPosition(transformedCursorIndex)
+    }
+
+    fun roundTransformedCursorIndex(direction: CursorAdjustDirection, transformedText: BigTextTransformed, compareWithPosition: Int, isOnlyWithinBlock: Boolean) {
+        transformedCursorIndex = roundedTransformedCursorIndex(transformedCursorIndex, direction, transformedText, compareWithPosition, isOnlyWithinBlock).also {
+            com.sunnychung.application.multiplatform.hellohttp.util.log.d { "roundedTransformedCursorIndex($transformedCursorIndex, $direction, ..., $compareWithPosition) = $it" }
+        }
+    }
+
+    fun roundedTransformedCursorIndex(transformedCursorIndex: Int, direction: CursorAdjustDirection, transformedText: BigTextTransformed, compareWithPosition: Int, isOnlyWithinBlock: Boolean): Int {
+        val possibleRange = 0 .. transformedText.length
+        val previousMappedPosition = transformedText.findOriginalPositionByTransformedPosition(compareWithPosition)
+        when (direction) {
+            CursorAdjustDirection.Forward, CursorAdjustDirection.Backward -> {
+                val step = if (direction == CursorAdjustDirection.Forward) 1 else -1
+                var delta = 0
+                while (transformedCursorIndex + delta in possibleRange) {
+                    if (transformedText.findOriginalPositionByTransformedPosition(transformedCursorIndex + delta) != previousMappedPosition) {
+                        return transformedCursorIndex + delta + if (isOnlyWithinBlock) {
+                            // for backward, we find the last index that is same as `previousMappedPosition`
+                            - step
+                        } else {
+                            // for forward, we find the first index that is different from `previousMappedPosition`
+                            0
+                        }
+                    }
+                    delta += step
+                }
+                // (transformedCursorIndex + delta) is out of range
+                return transformedCursorIndex + delta - step
+            }
+            CursorAdjustDirection.Bidirectional -> {
+                var delta = 0
+                while ((transformedCursorIndex + delta in possibleRange || transformedCursorIndex - delta in possibleRange)) {
+                    if (transformedCursorIndex + delta + 1 in possibleRange && transformedText.findOriginalPositionByTransformedPosition(transformedCursorIndex + delta + 1) != previousMappedPosition) {
+                        return transformedCursorIndex + delta + if (transformedCursorIndex + delta - 1 in possibleRange && transformedText.findOriginalPositionByTransformedPosition(transformedCursorIndex + delta - 1) == previousMappedPosition) {
+                            // position (transformedCursorIndex + delta) is a block,
+                            // while position (transformedCursorIndex + delta + 1) is not a block.
+                            // so return (transformedCursorIndex + delta + 1)
+                            1
+                        } else {
+                            0
+                        }
+                    }
+                    if (transformedCursorIndex - delta - 1 in possibleRange && transformedText.findOriginalPositionByTransformedPosition(transformedCursorIndex - delta - 1) != previousMappedPosition) {
+                        // for backward, we find the last index that is same as `previousMappedPosition`
+                        return transformedCursorIndex - delta //+ 1
+                    }
+                    ++delta
+                }
+                return transformedCursorIndex + delta - 1
+            }
+        }
+    }
+}
