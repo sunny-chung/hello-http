f05b2b6c977dc0106e952b4f9d93faaeb0eb91a1
Sunny Chung <sunnychung@live.hk>
2024-11-29 23:47:00 +0800

    fix removal of ktreesitter-json

--- src/jvmMain/kotlin/com/sunnychung/application/multiplatform/hellohttp/helper/InitNativeClasses.kt
+++ src/jvmMain/kotlin/com/sunnychung/application/multiplatform/hellohttp/helper/InitNativeClasses.kt
@@ -4,14 +4,12 @@ import io.github.dralletje.ktreesitter.graphql.TreeSitterGraphql
 import io.github.treesitter.ktreesitter.Language
 import io.github.treesitter.ktreesitter.Parser
 import io.github.treesitter.ktreesitter.Tree
-import io.github.treesitter.ktreesitter.json.TreeSitterJson
 
 class InitNativeClasses {
 
     init {
-        Parser(Language(TreeSitterJson.language())).also {
+        Parser(Language(TreeSitterGraphql.language())).also {
             val t: Tree = it.parse("")
         }
-        Language(TreeSitterGraphql.language())
     }
 }
